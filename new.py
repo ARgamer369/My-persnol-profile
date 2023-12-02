25e120803ec07c6b55cd041412bb4a174da64278
Abdur Rahman <mrargamer369@gmail.com>
2023-12-02 14:30:22 +0500

    gitignore

---
+++ new.py
@@ -0,0 +1,6 @@
+print("hello world")
+
+print("my name is {} and iam a {}") .format("Abdur Rahman")  	                                                                                                                1111111111111                                                                                                               Qur rahman","student"))
+
+list1 =["arbash ,farzeen ,azim , ashfaq"]
+
